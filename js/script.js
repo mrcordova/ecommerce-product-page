419aa44c513efb4d5330227efe6cbd06ec37c5aa
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-07-22 20:56:36 -0700

    logic to show current cart amount

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ let slideIndex = 1;
 const arrows = document.querySelectorAll(".left-arrow, .right-arrow");
 const counterBtns = document.querySelectorAll(".counter");
 const quantity = document.querySelector(".quantity-amount");
+const addToCartBtn = document.querySelector(".cart-btn");
+const cartAmount = document.querySelector(".cart-amount");
 
 showSlides(slideIndex);
 
@@ -25,12 +27,12 @@ function showSlides(n) {
     slideIndex = slides.length;
   }
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+    slides[i].classList.add("hide");
   }
   //   for (i = 0; i < dots.length; i++) {
   //     dots[i].className = dots[i].className.replace(" active", "");
   //   }
-  slides[slideIndex - 1].style.display = "block";
+  slides[slideIndex - 1].classList.toggle("hide", false);
   //   dots[slideIndex - 1].className += " active";
 }
 
@@ -38,7 +40,14 @@ function updateCounter(e) {
   const val = parseInt(e.currentTarget.getAttribute("data-value"));
   quantity.textContent = `${Math.max(0, parseInt(quantity.textContent) + val)}`;
 }
+function addToCart(e) {
+  const currentItemAmount =
+    parseInt(quantity.textContent) + parseInt(cartAmount.textContent);
 
+  cartAmount.classList.toggle("hide", currentItemAmount == 0);
+
+  cartAmount.textContent = `${currentItemAmount}`;
+}
 for (const arrow of arrows) {
   arrow.addEventListener("click", moveSlides);
 }
@@ -46,3 +55,4 @@ for (const arrow of arrows) {
 for (const counterBtn of counterBtns) {
   counterBtn.addEventListener("click", updateCounter);
 }
+addToCartBtn.addEventListener("click", addToCart);
